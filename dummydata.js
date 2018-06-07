35f00dbff2b42fe5c9a474b44a4d021e3f582bb6
Avram Billig <avram.billig@sema4genomics.com>
2018-06-07 13:13:14 -0400

    Initial commit

---
+++ dummydata.js
@@ -0,0 +1,175 @@
+var oracleAlphaData = {
+"medications": [
+  {
+    "name": "Crizotinib",
+    "start": "20-Mar-18",
+    "end": "20-Apr-18"
+  },
+  {
+    "name": "Ceritinib",
+    "start": "20-Feb-18",
+    "end": "02-Apr-18"
+  },
+  {
+    "name": "Lorlatinib",
+    "start": "01-May-18",
+    "end": "09-May-18"
+  },
+  {
+    "name": "3",
+    "start": "02-Jan-18",
+    "end": "05-Jan-18"
+  },
+  {
+    "name": "4",
+    "start": "20-Mar-18",
+    "end": "20-Apr-18"
+  },
+  {
+    "name": "5",
+    "start": "20-Feb-18",
+    "end": "02-Apr-18"
+  },
+  {
+    "name": "6",
+    "start": "01-May-18",
+    "end": "09-May-18"
+  },
+  {
+    "name": "Carboplatin-Pemetrexed",
+    "start": "02-Jan-18",
+    "end": "05-Jan-18"
+  },
+  {
+    "name": "Ceritinib",
+    "start": "20-Feb-18",
+    "end": "02-Apr-18"
+  },
+  {
+    "name": "Lorlatinib",
+    "start": "02-Nov-17",
+    "end": "02-Dec-17"
+  },
+  {
+    "name": "Ceritinib",
+    "start": "02-Jan-18",
+    "end": "10-Feb-18"
+  },
+  {
+    "name": "Ceritinib",
+    "start": "02-Nov-17",
+    "end": "10-Oct-17"
+  },
+  {
+    "name": "Carboplatin-Pemetrexed",
+    "start": "16-Dec-17",
+    "end": "18-Dec-17"
+  }
+],
+"basic_plotting_categories": [
+  {
+    "name": "Surgery1",
+    "description": "s1 description",
+    "date": "20-Feb-18",
+    "height": 5,
+    "category": "Surgery"
+  },
+  {
+    "name": "Surgery2",
+    "description": "s2 description",
+    "date": "20-Mar-18",
+    "height": 5,
+    "category": "Surgery"
+  },
+  {
+    "name": "Surgery3",
+    "description": "s2 description",
+    "date": "10-May-18",
+    "height": 5,
+    "category": "Surgery"
+  },
+  {
+    "name": "Surgery2",
+    "description": "s2 description",
+    "date": "11-Oct-17",
+    "height": 5,
+    "category": "Surgery"
+  },
+  {
+    "name": "Imaging1",
+    "description": "Imaging1 description",
+    "date": "11-Nov-17",
+    "height": 3,
+    "category": "Imaging"
+  },
+  {
+    "name": "Imaging2",
+    "description": "Imaging2 description",
+    "date": "04-Feb-18",
+    "height": 3,
+    "category": "Imaging"
+  },
+  {
+    "name": "Molecular1",
+    "description": "Molecular1 description",
+    "date": "04-Nov-17",
+    "height": 7,
+    "category": "Molecular"
+  },
+  {
+    "name": "Molecular2",
+    "description": "Molecular2 description",
+    "date": "04-Feb-18",
+    "height": 7,
+    "category": "Molecular"
+  },
+  {
+    "name": "Molecular3",
+    "description": "Molecular3 description",
+    "date": "04-May-18",
+    "height": 7,
+    "category": "Molecular"
+  },
+  {
+    "name": "radiation station",
+    "description": "Molecular3 description",
+    "date": "04-Jan-18",
+    "height": 7,
+    "category": "Radiation"
+  }
+],
+"line": [
+  {
+    "date": "20-Oct-17",
+    "value": 6
+  },
+  {
+    "date": "13-Nov-17",
+    "value": 1
+  },
+  {
+    "date": "18-Nov-17",
+    "value": 2
+  },
+  {
+    "date": "08-Mar-18",
+    "value": 4
+  },
+  {
+    "date": "08-Apr-18",
+    "value": 1
+  },
+  {
+    "date": "01-May-18",
+    "value": 3
+  },
+  {
+    "date": "08-Jun-18",
+    "value": 3
+  },
+  {
+    "date": "10-Jul-18",
+    "value": 2
+  }
+]
+}
